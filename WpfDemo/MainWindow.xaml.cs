c2776be5b809665a174aa9563198a4139a477c0a
janthony25 <junanthony.business@gmail.com>
2024-02-07 22:17:07 +1300

    Using cSharp to add and position tools

--- WpfDemo/MainWindow.xaml.cs
+++ WpfDemo/MainWindow.xaml.cs
@@ -19,6 +19,17 @@ namespace WpfDemo
         public MainWindow()
         {
             InitializeComponent();
+
+            Button myButton = new Button();
+            myButton.Content = "B";
+
+            Grid.SetRow(myButton, 3);
+            Grid.SetColumn(myButton, 4);
+
+
+            Grid myGrid = (Grid)FindName("myGrid");
+
+            myGrid.Children.Add(myButton);
         }
     }
 }
